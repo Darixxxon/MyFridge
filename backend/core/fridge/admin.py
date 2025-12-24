990045546f41fa6c1439511e3ecfe7e6ce2039a5
Darixxxon <dmorzuch@wp.pl>
2025-12-24 16:05:03 +0100

    Add model of product

--- backend/core/fridge/admin.py
+++ backend/core/fridge/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Fridge
+from .models import Fridge, Product
 
 
 # Register your models here.
@@ -14,3 +14,12 @@ class FridgeAdmin(admin.ModelAdmin):
     search_fields = ("name", "owner__username")
     list_filter = ("owner",)
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Product model.
+    """
+
+    list_display = ("name", "brand", "measurement", "unit", "barcode")
+    search_fields = ("name", "brand", "barcode")
+    list_filter = ("brand",)
--- backend/core/fridge/models.py
+++ backend/core/fridge/models.py
@@ -30,3 +30,30 @@ class Fridge(models.Model):
 
     def __str__(self):
         return f"Fridge {self.name}"
+
+class Product(models.Model):
+    """
+    Product model
+    """
+
+    UNITS = [
+        ("g", "Grams"),
+        ("kg", "Kilograms"),
+        ("ml", "Milliliters"),
+        ("l", "Liters"),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, default='')
+    brand = models.CharField(max_length=100, blank=True, default='')
+    measurement = models.DecimalField(decimal_places=2, max_digits=10)
+    unit = models.CharField(max_length=5, choices=UNITS)
+    barcode = models.CharField(max_length=13, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return f"Product {self.name} with barcode {self.barcode}"
