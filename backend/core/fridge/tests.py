d9f77ee2043e7d27b7086f74c015e933c3e5395e
Darixxxon <dmorzuch@wp.pl>
2025-12-24 14:00:47 +0100

    Small changes

--- backend/core/fridge/tests.py
+++ backend/core/fridge/tests.py
@@ -14,6 +14,28 @@ class FridgeModelTest(TestCase):
             owner=self.user
         )
 
+    def test_fridge_create(self):
+        self.assertEqual(Fridge.objects.count(), 1)
+
     def test_fridge_deleted_with_owner(self):
         self.user.delete()
         self.assertEqual(Fridge.objects.count(), 0)
+
+    def test_fridge_delete(self):
+        self.fridge.delete()
+        self.assertEqual(Fridge.objects.count(), 0)
+
+    def test_fridge_name(self):
+        self.assertEqual(self.fridge.name, "Test Fridge")
+
+    def test_fridge_description(self):
+        self.assertEqual(self.fridge.description, "")
+
+    def test_fridge_owner(self):
+        self.assertEqual(self.fridge.owner, self.user)
+
+    def test_fridge_str(self):
+        self.assertEqual(
+            str(self.fridge),
+            "Fridge Test Fridge"
+        )
