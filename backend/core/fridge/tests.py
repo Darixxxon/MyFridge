d291572e96c4b8b1845d466bb7e149b491cc35de
Darixxxon <dmorzuch@wp.pl>
2025-12-24 13:35:13 +0100

    Add tests to a Fridge model

--- backend/core/fridge/tests.py
+++ backend/core/fridge/tests.py
@@ -1,3 +1,23 @@
-# from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase
+from .models import Fridge
 
-# Create your tests here.
+class FridgeModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="test123",
+        )
+        print(f"User {self.user.username} created")
+
+        self.fridge = Fridge.objects.create(
+            name="Test Fridge",
+            owner=self.user
+        )
+        print(f"Fridge {self.fridge.name} created")
+
+    def test_fridge_deleted_with_owner(self):
+        self.user.delete()
+        print(f"User {self.user.username} deleted")
+        self.assertEqual(Fridge.objects.count(), 0)
+        print("Fridge deleted cascade")
