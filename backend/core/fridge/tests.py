30c0d85e4054139d420271f8f982427d148f6d74
Darixxxon <dmorzuch@wp.pl>
2025-12-24 20:12:33 +0100

    Add tests for a product model

--- backend/core/fridge/tests.py
+++ backend/core/fridge/tests.py
@@ -1,6 +1,10 @@
+from decimal import Decimal
+
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.test import TestCase
-from .models import Fridge
+from .models import Fridge, Product
+
 
 class FridgeModelTest(TestCase):
     def setUp(self):
@@ -39,3 +43,58 @@ class FridgeModelTest(TestCase):
             str(self.fridge),
             "Fridge Test Fridge"
         )
+
+class ProductModelTest(TestCase):
+    def setUp(self):
+        self.product_data = {
+            "name": "Test Product",
+            "brand": "Test Brand",
+            "measurement": Decimal("1.00"),
+            "unit": "l",
+            "barcode": "1234567890123",
+        }
+
+    def test_create_valid_product(self):
+        product = Product(**self.product_data)
+        product.full_clean()
+        product.save()
+        self.assertEqual(Product.objects.count(), 1)
+
+    def test_delete_product(self):
+        product = Product.objects.create(**self.product_data)
+        product.delete()
+        self.assertEqual(Product.objects.count(), 0)
+
+    def test_measurement_must_be_positive(self):
+        product = Product(**self.product_data)
+        product.measurement = Decimal("-1.00")
+        with self.assertRaises(ValidationError):
+            product.full_clean()
+
+    def test_name_required(self):
+        product = Product(**self.product_data)
+        product.name = ""
+        with self.assertRaises(ValidationError):
+            product.full_clean()
+
+    def test_invalid_unit(self):
+        product = Product(**self.product_data)
+        product.unit = ""
+        with self.assertRaises(ValidationError):
+            product.full_clean()
+
+    def test_barcode_required(self):
+        product = Product(**self.product_data)
+        product.barcode = ""
+        with self.assertRaises(ValidationError):
+            product.full_clean()
+
+    def test_barcode_unique(self):
+        Product.objects.create(**self.product_data)
+        product2 = Product(**self.product_data)
+        with self.assertRaises(ValidationError):
+            product2.full_clean()
+
+    def test_product_str(self):
+        product = Product.objects.create(**self.product_data)
+        self.assertEqual(str(product), "Product Test Product with barcode 1234567890123")
